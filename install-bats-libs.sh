d335c12aff94a8557950817c0ea30cbe065d1b6a
a-t-0 <you@example.com>
2022-12-25 12:50:32 +0100

    Simplified adding submodules.

--- install-bats-libs.sh
+++ install-bats-libs.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+git rm --cached test/libs/bats
+git rm --cached test/libs/bats-support
+git rm --cached test/libs/bats-assert
+
+rm -r test/libs
 mkdir -p test/libs
 
 git submodule add --force https://github.com/sstephenson/bats test/libs/bats
