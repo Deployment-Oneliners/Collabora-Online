dfb465897a17cd052fcbe7b60aa3339dcff3fc7f
name <example@example.com>
2023-05-25 01:39:43 +0200

    Changed cronjob time.

--- src/backup/create_cronjob.sh
+++ src/backup/create_cronjob.sh
@@ -6,7 +6,10 @@ source src/GLOBAL_VARS.sh
 
 # Clone the GitHub repository
 # TODO: pull if already cloned.
-git clone https://github.com/HiveMinds/Collabora-Online.git "$GIT_DIR_FOR_CRON"
+
+if [ ! -d "$GIT_DIR_FOR_CRON" ]; then
+  git clone https://github.com/HiveMinds/Collabora-Online.git "$GIT_DIR_FOR_CRON"
+fi
 
 # Assert manage_daily_backup.sh script exists.
 backup_manager_path="$GIT_DIR_FOR_CRON/src/backup/manage_daily_backup.sh"
@@ -20,10 +23,10 @@ chmod +x "$backup_manager_path"
 if crontab -l | grep -q "$backup_manager_path"; then
   echo "Cron job already exists. Skipping setup."
 else
-  # Add the cron job entry
+  # Add the cron job entry to run every day at 01:02 midnight.
   (
     crontab -l 2>/dev/null
-    echo "0 0 * * * $backup_manager_path"
+    echo "1 2 * * * $backup_manager_path"
   ) | crontab -
   echo "Cron job successfully set up."
 fi
