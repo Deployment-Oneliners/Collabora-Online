e97895da28ab6793831a805d9e945d2590171894
name <example@example.com>
2023-05-25 02:13:17 +0200

    Included git pull command.

--- src/backup/create_cronjob.sh
+++ src/backup/create_cronjob.sh
@@ -5,13 +5,19 @@
 source src/GLOBAL_VARS.sh
 
 # Clone the GitHub repository
-# TODO: pull if already cloned.
-current_path="$PWD"
 if [ ! -d "$GIT_DIR_FOR_CRON" ]; then
   git clone https://github.com/HiveMinds/Collabora-Online.git "$GIT_DIR_FOR_CRON"
 else
-  cd "$GIT_DIR_FOR_CRON" && git pull
-  cd "$current_path" || exit
+  sudo rm -r "$GIT_DIR_FOR_CRON"
+  if [ -d "$GIT_DIR_FOR_CRON" ]; then
+    echo "Error, $GIT_DIR_FOR_CRON dir still exists."
+    exit
+  fi
+  git clone https://github.com/HiveMinds/Collabora-Online.git "$GIT_DIR_FOR_CRON"
+  if [ ! -d "$GIT_DIR_FOR_CRON" ]; then
+    echo "Error, $GIT_DIR_FOR_CRON dir still exists."
+    exit
+  fi
 fi
 
 # Assert manage_daily_backup.sh script exists.
